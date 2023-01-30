97fc398771a5f360cdccdbd10f0b4c8d2e5b3c45
jjtheall <jtheall@quinnipiac.edu>
2023-01-30 18:57:21 -0500

    initial commit

---
+++ src/test/java/com/csc492/JunkademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc492;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunkademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

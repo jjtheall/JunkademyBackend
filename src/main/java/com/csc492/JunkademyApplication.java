ced0f161bf9bebf0486677a042e89731f6425c54
jjtheall <jtheall@quinnipiac.edu>
2023-02-08 20:30:14 -0500

    code block list prototype working, added api endpoint to return list of blocks

--- src/main/java/com/csc492/JunkademyApplication.java
+++ src/main/java/com/csc492/JunkademyApplication.java
@@ -1,5 +1,8 @@
 package com.csc492;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,19 @@ public class JunkademyApplication {
 	@GetMapping("/home")
 	public String homepageText() {
 		return "Junkademy Homepage";
+	}
+	
+	@GetMapping("/codeblock")
+	public List<CodeBlock> codeBlockTest() {
+		List<CodeBlock> blockList = new ArrayList<CodeBlock>();
+		
+		blockList.add(new CodeBlock(1));
+		blockList.add(new CodeBlock(2));
+		blockList.add(new CodeBlock(3));
+		blockList.add(new CodeBlock(4));
+		blockList.add(new CodeBlock(5));
+		
+		return blockList;
 	}
 
 }

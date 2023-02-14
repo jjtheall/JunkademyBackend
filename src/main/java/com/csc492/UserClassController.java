32d504a8c43ba905d80f312107f9459f18a9bf45
jjtheall <jtheall@quinnipiac.edu>
2023-02-13 19:30:54 -0500

    UserClassController.java written

---
+++ src/main/java/com/csc492/UserClassController.java
@@ -0,0 +1,33 @@
+package com.csc492;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserClassController {
+
+	@Autowired
+	private UserClassService userClassService;
+	
+	@GetMapping
+	@RequestMapping(path="user/get")
+	@ResponseStatus(HttpStatus.OK)
+	public UserClass fetchUserByName(String username) {
+		return userClassService.fetchUserByName(username);
+	}
+	
+	@PostMapping
+	@RequestMapping(path="user/save")
+	@ResponseStatus(HttpStatus.OK)
+	public UserClass saveUser(@RequestBody UserClassDTO userClassDTO) {
+		return userClassService.saveUser(userClassDTO);
+	}
+	
+	
+}

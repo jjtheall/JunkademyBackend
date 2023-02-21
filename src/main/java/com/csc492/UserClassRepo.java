d308752bf74bfe7e580b184e32395e0f93deb409
jjtheall <jtheall@quinnipiac.edu>
2023-02-20 22:18:37 -0500

    backend connection to gcp database instance

--- src/main/java/com/csc492/UserClassRepo.java
+++ src/main/java/com/csc492/UserClassRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserClassRepo extends CrudRepository<UserClass, Integer>{
 	
-	@Query(value="SELECT * FROM Users WHERE username = :username", nativeQuery = true)
+	@Query(value="SELECT * FROM users WHERE username = :username", nativeQuery = true)
 	UserClass fetchUserByName(@Param("username") String username);
 
 }
